eee17a8224c2e8b43d4811610b172004c5c796b8
Aamir Bhat <62208734+aamirbhat382@users.noreply.github.com>
2021-03-09 23:13:00 +0530

    Add path

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const butInstall = document.getElementById('butInstall');
 
 /* Only register a service worker if it's supported */
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./service-worker.js');
+    navigator.serviceWorker.register('/service-worker.js');
 }
 
 /**
